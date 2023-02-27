23a4abc30dfa64f1718f736d5aa15d6e011a42de
xiongguangsen02 <3096429133@qq.com>
2023-02-27 17:56:17 +0800

    新增登录页-头部、底部绘制

--- src/views/layouts/UserLayout.vue
+++ src/views/layouts/UserLayout.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-02-22 10:56:04
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-02-23 15:15:51
+ * @LastEditTime: 2023-02-27 15:59:26
  * @FilePath: \测试用vue\vue-demo\src\views\layouts\UserLayout.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,19 +12,24 @@
       <div class="user-layout-content">
         <div class="header">
           <div class="top">
-            <a href="/">
+            <a href="/user">
               <!-- <img src="~@/assets/logo.svg" alt="antdesign图标"> -->
-              <img src="~@/assets/logo.svg" alt="antdesign图标" class="logo">
+              <img src="~@/assets/logo.svg" alt="antdesign图标" class="logo" />
               <span class="title">Ant Design</span>
             </a>
           </div>
-          <div class="dec">
-            Ant Design
-          </div>
+          <div class="dec">Ant Design is the most influential web design specification in Xihu district</div>
         </div>
         <router-view />
         <div class="footer">
-
+          <div class="top-text">
+            <a href="/user">帮助</a>
+            <a href="/user">隐私</a>
+            <a href="/user">条款</a>
+          </div>
+          <div class="bottom-text">
+            Copyright © 2018 vueComponent
+          </div>
         </div>
       </div>
     </div>
@@ -35,10 +40,39 @@
 export default {
   name: "UserLayout",
   methods: {
-    
+    // consoleMethod() {
+    //   let objA = {
+    //     a: 1,
+    //     b: 2,
+    //     c: {
+    //       d: 6,
+    //     },
+    //     g: {
+    //       gg: 99
+    //     }
+    //   };
+
+    //   console.log(111,objA);
+
+    //   let objB = objA;
+
+    //   let {a, b, c, g} = objA
+
+    //   a=44
+    //   g.gg = 11
+    //   console.log(g);
+
+    //   objB.b = 3;
+    //   objB.c = { f: 7 };
+
+    //   console.log(222,objA);
+
+    //   console.log(333,objB);
+    // },
   },
   created() {
-  }
+    // this.consoleMethod()
+  },
 };
 </script>
 
@@ -59,25 +93,49 @@ export default {
         .top {
           height: 44px;
           line-height: 44px;
-          
+
           .logo {
             height: 44px;
             vertical-align: top;
+            margin-right: 16px;
           }
 
           .title {
             font-size: 33px;
-            color: rgba(0, 0, 0, .85);
-            font-family: Avenir, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+            color: rgba(0, 0, 0, 0.85);
+            font-family: Avenir, "Helvetica Neue", Arial, Helvetica, sans-serif;
             font-weight: 600;
-            // position: relative;
-            // top: 2px;
+            position: relative;
+            top: 2px;
           }
         }
 
         .dec {
-          line-height: 60px;
-          font-size: 16px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 14px;
+          margin: 12px auto 40px;
+        }
+      }
+
+      .footer {
+        margin: 48px auto 24px;
+        text-align: center;
+
+        .top-text {
+          margin-bottom: 8px;
+
+          a {
+            color: rgba(0, 0, 0, 0.45);
+            font-size: 14px;
+            &:not(:last-child) {
+              margin-right: 40px;
+            }
+          }
+        }
+
+        .bottom-text {
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 14px;
         }
       }
     }
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-02-22 10:41:45
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-02-23 17:52:54
+ * @LastEditTime: 2023-02-24 15:26:47
  * @FilePath: \测试用vue\vue-demo\src\views\user\Login.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -11,7 +11,7 @@
     <!-- 登录页面 -->
     <div class="line-box">
       <span class="inline-blockOne"></span>
-      <span class="inline-blockTwo"></span>
+      <!-- <span class="inline-blockTwo"></span> -->
       x
     </div>
   </div>
@@ -26,6 +26,7 @@ export default {
 <style lang="less" scoped>
   .line-box {
     background: #fff;
+    // padding-top: 20px;
     span {
       display: inline-block;
       height: 40px;
@@ -36,13 +37,13 @@ export default {
       // margin-top: 20px;
     }
     .inline-blockOne {
-      margin-top: 20px;
-      vertical-align: text-top;
+      // margin-top: 20px;
+      vertical-align: top;
       
     }
-    // .inline-blockTwo {
-    //   margin-top: 20px;
-    //   vertical-align: text-top;
-    // }
+    .inline-blockTwo {
+      margin-top: 30px;
+      vertical-align: text-top;
+    }
   }
 </style>

58d080f7851aaf30a5f7031ed447517af2072a5e
xiongguangsen02 <3096429133@qq.com>
2023-04-04 16:23:51 +0800

    新增第二次打印

--- src/views/layouts/UserLayout.vue
+++ src/views/layouts/UserLayout.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-02-22 10:56:04
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-04-04 16:19:50
+ * @LastEditTime: 2023-04-04 16:23:29
  * @FilePath: \测试用vue\vue-demo\src\views\layouts\UserLayout.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -39,6 +39,7 @@ export default {
   },
   created() {
     console.log(111111111111111111777777777777777);
+    console.log(77777777777777,222222);
   }
 };
 </script>
